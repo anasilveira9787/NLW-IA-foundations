0537442d7c860f507251ae4d058177bfce59cc6a
Ana Silveira <ana.ccord@gmail.com>
2023-09-14 19:21:55 -0300

    aula 3

--- server/download.js
+++ server/download.js
@@ -1,7 +1,7 @@
 import ytdl from 'ytdl-core'
 import fs from 'fs'
 
-export const download = (videoId) => {
+export const download = (videoId) => new Promise((resolve, reject) => {
 
   const videoURL = "https://www.youtube.com/shorts/" + videoId
   console.log(`Realizando o download do vídeo: ${videoId}`)
@@ -17,8 +17,13 @@ export const download = (videoId) => {
   })
   .on("end", () => {
     console.log('Download do vídeo finalizado')
+    resolve()
   })
   .on("error", (error) => {
-    console.log('Não foi possível fazer o download do vídeo. Erro:', error)
-  }).pipe(fs.createWriteStream("./tmp/audio.mp4"))
-}
+    console.log('Não foi possível fazer o download do vídeo. Erro:', 
+    error
+    )
+    reject(error)
+  })
+  .pipe(fs.createWriteStream("./tmp/audio.mp4"))
+})
--- web/main.js
+++ web/main.js
@@ -1,3 +1,5 @@
 import "./styles/base.css"
 import "./styles/app.css"
 import "./styles/form.css"
+
+import "./form.js"
